94fafb055d4384d9e4b25784e92ce056c8737f2c
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-25 18:41:16 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int boy, girl;
+        boy = 18;
+        girl = 68;
+
+        //&& = both have to be true
+        // || = only one has to be true
+        if(boy > 10 && girl <60){
+            System.out.println("enter");
+        }
+        else{
+            System.out.println("do not enter");
+        }
+
+    }
+}
